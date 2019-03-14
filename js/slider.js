666f47dad990213355b94b5cd499fbb20aff83bc
Denis Lozitskiy <denis.lozitskiy@mail.ru>
2019-03-14 10:46:29 +0300

    Applied ternary operator; Added comments // Применён тернарный оператор; Добавлены комментарии

--- js/slider.js
+++ js/slider.js
@@ -4,15 +4,10 @@
     let slider_btns = document.querySelectorAll(".slider_nav");
     const TIME = 10000;
     
-    (function init() {
+    (function init() { // Function will start autamaticly || Функция запустится автоматически
         slider_btns[i].checked = "checked";
-  
-        if (i < slider_btns.length - 1) {
-            i++;
-        } else {
-            i = 0;
-        }
+        (i < slider_btns.length - 1) ? i++ : i = 0; // slider_btns.length = 3
 
-        setTimeout(init, TIME)
+        setTimeout(init, TIME) // Loops the function || Зацикливает функцию
     })();
 })();
