fa5d3e5a346235cc820f2f276a2f1fd99de15d0f
Denis Lozitskiy <denis.lozitskiy@mail.ru>
2019-03-16 07:02:56 +0300

    Added comments // Добавлены комментарии

--- js/events.js
+++ js/events.js
@@ -1,11 +1,11 @@
 "use strict";
 (function() {
-    //=============SHOW/HIDE MOBILE MENU
+    //=============SHOW/HIDE MOBILE MENU || СПРЯТАТЬ/ПОКАЗАТЬ МОБИЛЬНОЕ МЕНЮ
     document.querySelector(".btn-showmenu").addEventListener("click", function() {
         document.querySelector(".closed-menu").classList.toggle("opened-menu");
     });
 
-    //=============MENU SMOOTH SCROLL
+    //=============SMOOTH SCROLL OF MENU LINKS || ПЛАВНОЕ ПРОКРУЧИВАНИЕ ССЫЛОК МЕНЮ
     Array.from(document.querySelectorAll(".container_menu ul a")).forEach(function(item) {
         item.addEventListener("click", function(evt) {
             let href = item.getAttribute("href");
@@ -15,14 +15,15 @@
         })
     });
     
-    //=============SHOW MORE FEATURE ITEMS
+    //=============SHOW REMAINING FEATURE ITEMS || ПОКАЗЫВАЕТ ОСТАВШИЕСЯ FEATURE ПРЕДМЕТЫ
     document.querySelector(".btn-feature").addEventListener("click", function () { 
-   	    this.style.display = "none";
+   	    this.style.display = "none"; // Hides btn-feature || Прячет btn-feature
+   	    
    	    Array.from(document.querySelectorAll(".item_feature:nth-child(even)")).forEach(function(item) {
             item.style.display = "flex";
         })
     });
-    //=============SHOW/HIDE MAP
+    //=============SHOW/HIDE MAP || СПРЯТАТЬ/ПОКАЗАТЬ МОБИЛЬНОЕ МЕНЮ
     document.querySelector(".map_button").addEventListener("click", function() { 
         document.querySelector("#map").classList.toggle("opened-map");
     });  
